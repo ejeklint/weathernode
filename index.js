74b46604b1fdb2a2d125f8f9be5fcdfdcec3c12e
Per Ejeklint <ejeklint@me.com>
2012-02-11 17:29:18 +0100

    File rename and less ugly exports blah blah

--- index.js
+++
@@ -1,5 +0,0 @@
-var PachubeLogger = require('./lib/pachube_logger').PachubeLogger;
-var dataLogger = new PachubeLogger();
-
-var OS_WStation = require('./lib/os_wstation').OS_WStation;
-var weatherStation = new OS_WStation(dataLogger);
---
+++ server.js
@@ -0,0 +1,5 @@
+var PachubeLogger = require('./lib/pachube_logger');
+var dataLogger = new PachubeLogger();
+
+var OS_WStation = require('./lib/os_wstation');
+var weatherStation = new OS_WStation(dataLogger);
