38e77bc65db8b78464c70ece12085a6cf7894862
Per Ejeklint <ejeklint@me.com>
2012-08-18 17:57:20 +0200

    Rename to Cosm. Clean out old dead code.

--- server.js
+++ server.js
@@ -1,5 +1,2 @@
-//var PachubeLogger = require('./lib/pachube_logger');
-//var dataLogger = new PachubeLogger();
-
 var OS_WStation = require('./lib/os_wstation');
-var weatherStation = new OS_WStation(/*dataLogger*/);
+var weatherStation = new OS_WStation();
